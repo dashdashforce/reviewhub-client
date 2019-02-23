14efbe2cde0e261f69d569146051c60cf10ab150
Maks <maks@MacBook-Pro-van-Bintime.local>
2019-02-23 20:29:16 +0200

    add github auth flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import ApolloClient from 'apollo-client';
+import {ApolloProvider} from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import GitHubLogin from './components/github-login';
+import Home from './components/home';
+import {token, isAuth, API_URL, getLink} from './auth';
 import './App.css';
 
-class App extends Component {
+const GITHUB_CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID
+const GITHUB_REDIRECT_URI = process.env.REACT_APP_GITHUB_REDIRECT_URI
+
+
+interface GithubResponse {
+  code: string;
+}
+
+class App extends Component<any, any> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isSignedIn: isAuth,
+      authToken: token,
+    };
+  }
+
+  handleOnLoginSuccess(response: GithubResponse) {
+    console.log(response);
+    axios.post(`${API_URL}/auth`, response)
+      .then((response: any) => {
+        localStorage.setItem('token', response.data.token)
+        this.setState({
+          isSignedIn: !(response.data.token === null),
+          token: response.data.token
+        });
+      });
+  }
+
+  handleOnLoginFailed(response: any) {
+    console.log(response);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      {
+        !this.state.isSignedIn
+          ? 
+          <ApolloProvider client={new ApolloClient({
+            link: getLink(this.state.token),
+            cache: new InMemoryCache()
+          })}>
+          <Home />
+        </ApolloProvider>
+          :
+        <GitHubLogin
+          clientId={GITHUB_CLIENT_ID}
+          onSuccess={this.handleOnLoginSuccess.bind(this)}
+          onFailure={this.handleOnLoginSuccess.bind(this)}
+          redirectUri={GITHUB_REDIRECT_URI}
+          scope="repo"
+        />
+      }
       </div>
     );
   }
